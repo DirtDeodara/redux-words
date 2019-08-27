014052f6442e5cfa2d80c8fb9bb086754300a15f
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-27 16:03:22 -0700

    brought update-search-term into wordReducer

--- src/reducers/wordsReducer.js
+++ src/reducers/wordsReducer.js
@@ -1,9 +1,15 @@
 import words from '../fixtures/words.json';
+import { UPDATE_SEARCH_TERM } from '../actions/wordActions';
 
 const initialState = {
-  dictionary: [...words]
+  dictionary: [...words],
+  searchTerm: ''
 };
 
 export default function wordsReducer(state = initialState, action) {
+  switch(action.type) {
+    case UPDATE_SEARCH_TERM:
+      return { ...state, searchTerm: action.payload };
+  }
   return state;
 }
