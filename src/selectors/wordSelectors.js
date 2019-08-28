3741570a4d1d7c2cda774e2cbbcff8dd19c1d767
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-27 17:01:21 -0700

    created new selectors to use a searchTerm to filter results

--- src/selectors/wordSelectors.js
+++ src/selectors/wordSelectors.js
@@ -3,3 +3,14 @@ export const getWords = state => state.words.dictionary;
 export const getFirstWords = (state, count = 1000) => {
   return getWords(state).slice(0, count);
 };
+
+export const getSearchTerm = (state) => {
+  return state.words.searchTerm;
+};
+
+export const getTopWordsByTerm = (state, count) => {
+  const searchTerm = getSearchTerm(state);
+  return getWords(state).filter(word => {
+    return word.includes(searchTerm);
+  }).slice(0, count);
+};
